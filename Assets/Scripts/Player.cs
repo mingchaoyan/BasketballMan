2ad1ba991b642975ee70252c881b6a8368e4aad5
mingchaoyan <mingchaoyan@gmail.com>
2014-09-28 20:40:34 +0800

    实现篮球投出

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,9 +5,15 @@ public class Player : MonoBehaviour {
 
     float speed = 25.0f;
 
+    public Rigidbody TempBasketball;
+    public Transform hand;
+    Rigidbody Basketball;
+
 	// Use this for initialization
 	void Start () {
-	
+        Basketball = Instantiate(TempBasketball, hand.position, hand.rotation) as Rigidbody;
+        Basketball.transform.parent = hand;
+        Basketball.rigidbody.useGravity = false;
 	}
 	
 	// Update is called once per frame
@@ -23,6 +29,7 @@ public class Player : MonoBehaviour {
         else if(Input.GetButtonUp("Play"))
         {
             gameObject.animation.PlayQueued("fire");
+            Basketball.transform.parent = null;
             gameObject.animation.PlayQueued("idle");
         }
 	
