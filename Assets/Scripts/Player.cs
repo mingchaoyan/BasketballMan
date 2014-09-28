76bcf8d28ffedde58956aea4fd79291d32c9e782
mingchaoyan <mingchaoyan@gmail.com>
2014-09-28 20:07:34 +0800

    添加主角旋转

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+    float speed = 25.0f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,12 +12,14 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        //if (Input.GetKeyDown(KeyCode.A))
+        gameObject.transform.RotateAround(
+            new Vector3(0.09215456f, -0.8153586f, 3.342688f),
+            new Vector3(0, 1.0f, 0),
+            speed*Time.deltaTime*Input.GetAxis("Horizontal"));
         if(Input.GetButtonDown("Play"))
         {
             gameObject.animation.PlayQueued("aim");
         }
-        //else if (Input.GetKeyUp(KeyCode.A))
         else if(Input.GetButtonUp("Play"))
         {
             gameObject.animation.PlayQueued("fire");
