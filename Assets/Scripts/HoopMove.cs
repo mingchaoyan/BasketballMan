9467afe4feba6ae2a864bd220a7328caa9863e8b
mingchaoyan <mingchaoyan@gmail.com>
2014-09-28 23:07:58 +0800

    使篮板自动移动，增加游戏难度

---
+++ Assets/Scripts/HoopMove.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoopMove : MonoBehaviour {
+    public float speed = 0.3f;
+    bool moveLeft = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Player.isPlay)
+        {
+            if (moveLeft)
+                transform.Translate(-Vector3.left*speed*Time.deltaTime);
+            else
+                transform.Translate(Vector3.left*speed*Time.deltaTime);
+
+            if (transform.position.x > 2.92)
+                moveLeft = false;
+            if (transform.position.x < -2.92)
+                moveLeft = true;
+        }
+	
+	}
+}
