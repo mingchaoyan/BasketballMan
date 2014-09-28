7b65d584729b08d14155878e5fe5f373e45eca2b
mingchaoyan <mingchaoyan@gmail.com>
2014-09-28 22:10:52 +0800

    检测投中

---
+++ Assets/Scripts/ThroughHoop.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThroughHoop : MonoBehaviour {
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "prefabBasketball(Clone)")
+        {
+            Debug.Log("hit");
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
