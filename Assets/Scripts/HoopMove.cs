cc89953f03bcb01547e30fb3b085834b41ca2295
mingchaoyan <mingchaoyan@gmail.com>
2014-10-19 23:45:49 +0800

    修正风格

--- Assets/Scripts/HoopMove.cs
+++ Assets/Scripts/HoopMove.cs
@@ -1,3 +1,4 @@
+<<<<<<< Upstream, based on origin/master
 ﻿using UnityEngine;
 using System.Collections;
 
@@ -29,3 +30,35 @@ public class HoopMove : MonoBehaviour
 	
     }
 }
+=======
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoopMove : MonoBehaviour
+{
+    public float speed = 0.3f;
+    bool moveLeft = true;
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+    }
+	
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Player.isPlay) {
+            if (moveLeft)
+                transform.Translate (-Vector3.left * speed * Time.deltaTime);
+            else
+                transform.Translate (Vector3.left * speed * Time.deltaTime);
+
+            if (transform.position.x > 2.92)
+                moveLeft = false;
+            if (transform.position.x < -2.92)
+                moveLeft = true;
+        }
+    }
+}
+>>>>>>> 103514c 修正风格
